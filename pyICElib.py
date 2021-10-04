69cda55baf965b09425dc1f6e086c08a3c3948a4
Alexandre Vaudrey <alexandre.vaudrey@gmail.com>
2021-10-04 15:55:53 +0200

    Creation of a new class called SimpleChemicalReaction which is dedicated to quantitative calculations related to chemical reactions (extent of reaction, molar concentration and so on).

--- pyICElib.py
+++ pyICElib.py
@@ -25,6 +25,93 @@ __docformat__ = "restructuredtext en"
 __author__ = "Alexandre Vaudrey <alexandre.vaudrey@gmail.com>"
 __date__ = "18/09/2017"
 
+class SimpleChemicalReaction:
+    """
+    Chemical reaction transforming a set of reactants in a set of products. Each
+    set is represented as a dictionary of type 'set = {'Compound1':(nu1, n1(0)),
+    ...}' with:
+        - 'Compound1' the chemical formula of compound 1, as e.g.  'CH4', 'H2',
+        'O2', 'H2Ov' (for water vapour), 'H2Ol' (for liquid water), etc.
+        - nu1 the (always) positive stoichiometric coefficient of compound 1.
+        - n1(0)>0 the initial molar amount of compound 1, in mol.
+    """
+    # Attributes
+    def __init__(self):
+        # If it's necessary to give a name to the engine geometry 
+        self.name = 'reaction1'
+        # Set of reactants, here for example hydrogen and oxygen in
+        # stoichiometric proportions
+        self.reactants = {'H2':(1.0,1.0), 'O2':(0.5,0.5)}
+        # Set of products, here only water vapour
+        self.products = {'H2Ov':(1.0,0.0)}
+    # Methods
+    def maximum_extent_of_reaction(self):
+        """
+        Calculate the maximum extent of reaction that can be reached according
+        to the initial amounts of reactants available.
+        """
+        # Calculation of the maximum reaction progress corresponding to each
+        # reactant
+        list_of_xi_max = []
+        for values in self.reactants.values():
+            if (values[0] != 0):
+                list_of_xi_max.append(values[1]/values[0])
+        # And return of the minimum
+        return min(list_of_xi_max)
+    def molar_amounts(self, relative_xi):
+        """
+        Calculation of the molar amounts of chemical compounds at an
+        intermediate extent of reaction represented by 'relative_xi' with:
+            relative_xi = extent/maximum_extent
+        """ 
+        if (relative_xi < 0) or (relative_xi > 1.0):
+            raise ValueError('Relative extent of reaction is from 0% to 100%')
+        else:
+            # Maximum extent of reaction
+            xi_max = self.maximum_extent_of_reaction()
+            # Dictionary containing the final molar amounts of reactants and
+            # products
+            amounts = {}
+            # We start with the calculation of final amounts of reactants
+            for react, values in zip(self.reactants.keys(),\
+                                     self.reactants.values()):
+                amounts[react] = values[1]-relative_xi*xi_max*values[0]
+                # And for the products
+            for prod, values in zip(self.products.keys(),\
+                                    self.products.values()):
+                amounts[prod] = values[1]+relative_xi*xi_max*values[0]
+            return amounts
+    def final_molar_amounts(self):
+        """
+        Final molar amounts of reactants and products.
+        """
+        # The final composition is just the one of reaction but with a 100%
+        # relative extent of reaction.
+        return self.molar_amounts(1.0)
+    def molar_concentrations(self, relative_xi):
+        """ 
+        Molar concentration of each compound in the reactive mixture at an
+        intermediate extent of reaction represented by 'relative_xi' with:
+            relative_xi = extent/maximum_extent
+        """ 
+        if (relative_xi < 0) or (relative_xi > 1.0):
+            raise ValueError('Relative extent of reaction is from 0% to 100%')
+        else:
+            # Molar quantities
+            n = self.molar_amounts(relative_xi)
+            # Total amount of moles at the end of the reaction
+            ntot = sum(list(n.values()))
+            # Final concentrations
+            x = {}
+            for elem, ni in zip(n.keys(), n.values()):
+                x[elem] = ni/ntot
+            return x
+    def final_molar_concentrations(self):
+        """
+        Molar concentration of each components in the final mixture.
+        """
+        return self.molar_concentrations(1.0)
+
 class EngineCylinder:
     """ 
     Main geometrical and operating parameters of the concerned engine
@@ -51,11 +138,13 @@ class EngineCylinder:
         self.compression_ratio = 10.
         # Connecting rod length, in [mm] 
         self._connecting_rod_length = 130.
-        # Angles at which the intake valve is open and closed, respectively.
-        # Theses values correspond to ideal Beau de Rochas and Diesel cycles.
+        # Angles at which the intake valve opening and closing process start,
+        # respectively. Theses values correspond to ideal Beau de Rochas and
+        # Diesel cycles.
         self.IVO_angle = -360.
         self.IVC_angle = -180.
-        # Angles at which the exhaust valve is open and closed, respectively
+        # Angles at which the exhaust valve starts to open and closed,
+        # respectively.
         self.EVO_angle = 180.
         self.EVC_angle = 360.
     # Connecting rod length is defined as a property to avoid any mechanical
@@ -112,140 +201,10 @@ class EngineCylinder:
         dVdtheta = 0.5*self.displaced_volume()*(np.sin(radangle)\
                                               +0.5*ratio*np.sin(2*radangle))
         return dVdtheta*np.pi/180.
-    # FIXME: these two methods must be done taking into account the periodic
-    # motion of the crank
-    def is_intake_valve_open(self, crank_angle):
-        """ Is the intake valve open at this crank angle. """
-        if (crank_angle < self.IVC_angle):
-            IV_open = True
-        else:
-            IV_open = False
-        return IV_open
-    def is_exhaust_valve_open(self, crank_angle):
-        """ Is the intake valve open at this crank angle. """ 
-        if (crank_angle < self.EVC_angle):
-            EV_open = True
-        else:
-            EV_open = False
-        return EV_open
-
-# === Old version, to cancel once used ========================================
-class EngineGeometry:
-    """
-    Geometric model of a reciprocating internal combustion engine.
-    """
-    # The default values of these attributes are coming from an existing
-    # 4-strokes and 4-cylinders spark ignited engine from NISSAN.
-    def __init__(self):
-        # If it's necessary to give a name to the engine geometry 
-        self.name = 'geometry1'
-        # Number of cylinders
-        self.number_of_cylinders = 4
-        # Diameter of each cylinder, in mm.
-        self.bore = 73.6
-        # Stroke
-        self.stroke = 88.0
-        # Compression ratio
-        self.compression_ratio = 9.5
-        # Ratio of connecting rod length to the crank radius, sometimes a
-        # starting parameter
-        self._connecting_rod_ratio = 3.0
-        # Sometimes, the connecting rod length is known
-        self._connecting_rod_length = 132.0
-    # Attributes defined as properties
-    @property
-    def connecting_rod_ratio(self):
-        """ Ratio of the connecting rod length to the crank radius,
-        dimensionless. """
-        return self._connecting_rod_ratio
-    @connecting_rod_ratio.setter
-    def connecting_rod_ratio(self, R):
-        """ New value of the Ratio of the connecting rod length to the crank
-        radius, dimensionless. """
-        # A wrong value of this parameter, because of a possible confusion with
-        # its inverse --- which is as much often used as this one --- would
-        # create NaN values in the rest.
-        if R<=1.0:
-            raise ValueError("The 'connecting rod ratio' is the ratio of the"
-                             "connecting rod length to the crank radius, it"
-                             "must be greater than one !")
-        self._connecting_rod_ratio = R
-        # And the related connecting rod length
-        self._connecting_rod_length = 0.5*self._connecting_rod_ratio*self.stroke
-        pass
-    @property
-    def connecting_rod_length(self):
-        """ Length of the connecting rod, in mm. """
-        return self._connecting_rod_length
-    @connecting_rod_length.setter
-    def connecting_rod_length(self, l):
-        """ New length of the connecting rod, in mm. """
-        # Check if the entered value is actually greater than the stroke
-        if (l <= self.stroke):
-            raise ValueError("The connecting rod length cannot be lower than"
-                             "the stroke.")
-        self._connecting_rod_length = l
-        # Calculation of the resulting connecting rod ratio
-        self._connecting_rod_ratio = l/(0.5*self.stroke)
-        pass
-    # And other methods
-    def displaced_volume(self):
-        """ Total displaced volume, or swept volume of the engine, in liter. """
-        return 1e-6*self.number_of_cylinders*0.25*np.pi*pow(self.bore,2)*\
-                self.stroke
-    def clearance_volume(self):
-        """ Total clearance volume of the engine, in liter. """
-        return self.displaced_volume()/(self.compression_ratio-1.0)
-    def maximum_volume(self):
-        """The maximum volume inside the cylinders, equal to the sum of the
-        displaced volume and of the clearance one, is used as an horizontal
-        limit within the indicated diagram."""
-        return self.displaced_volume()+self.clearance_volume()
-    def piston_position(self, angle):
-        """ Relative position of the piston, =1 at TDC and =0 at BDC, regarding
-        to the crank angle in degres. """
-        # Angle in radians
-        radangle = np.radians(angle)
-        # Ratio of the crank radius on the connecting rod length
-        ratio = 1/self.connecting_rod_ratio
-        return 1-0.5*((1-np.cos(radangle))+\
-                      (1-np.sqrt(1-pow(ratio*np.sin(radangle),2)))/ratio)
-
-class EngineCycle(EngineGeometry):
-    """Internal cylinder pressure vs. crank angle data series."""
-    # TODO : Computation of the mean indicated pressure using the (p,V) datas
-    def __init__(self):
-        super(EngineCycle, self).__init__()
-        # Name of the cycle if required
-        self.cycle_name = 'cycle1'
-        # List of angles values
-        self.angle = [-360,-180,0,180,360]
-        # List of corresponding pressure values inside the cylinder
-        self.pressure = [1.0,1.0,2.46,1.0,1.0]
-        # Ratio of connecting rod length to the crank radius, sometimes a
-        # starting parameter
-        connecting_rod_ratio =\
-                property(EngineGeometry.connecting_rod_ratio.__get__)
-        # Sometimes, the connecting rod length is known
-        connecting_rod_length =\
-                property(EngineGeometry.connecting_rod_length.__get__)
-    def piston_position(self):
-        """ Relative position of the piston, =1 at TDC and =0 at BDC, regarding
-        to the crank angle in degres. """
-        # Angle in radians
-        radangle = np.radians(self.angle)
-        # Ratio of the crank radius on the connecting rod length
-        ratio = 1/self.connecting_rod_ratio
-        return 1-0.5*((1-np.cos(radangle))+\
-                      (1-np.sqrt(1-pow(ratio*np.sin(radangle),2)))/ratio)
-    def actual_volume(self):
-        """Actual volume in the cylinders, in liters."""
-        return self.clearance_volume()+(1-self.piston_position())*\
-                self.displaced_volume()
 
 if __name__ == '__main__':
-    cylindre1 = EngineCylinder()
-    print(cylindre1.__dict__)
+    pass
+#    cylindre1 = EngineCylinder()
     # Technical parameters of the engine
 #    cummins = EngineCycle()
 #    cummins.name = 'Cummins QSB6.7'
